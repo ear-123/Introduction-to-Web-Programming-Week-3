5ae23d3742f79c4e4fe0a516c8dc31ef949db250
ear-123 <elias.a.rajala@gmail.com>
2024-06-19 11:51:36 +0300

    s

--- js/skript.js
+++ js/skript.js
@@ -6,10 +6,16 @@ document.addEventListener("DOMContentLoaded", function(){
 async function code(){
     const tbody = document.getElementById("tbody")
 
-    const url = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff"
-    const dataPromise = await fetch(url)
+    const url1 = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff"
+    const dataPromise = await fetch(url1)
     const dataJSON = await dataPromise.json()
     //console.log(dataJSON)
+
+    const url2 ="https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065"
+    const dataPromise2 = await fetch(url2)
+    const dataJSON2 = await dataPromise2.json()
+    console.log(dataJSON2);
+
 
     const municipality = dataJSON.dataset.dimension.Alue.category.label
     
@@ -18,12 +24,15 @@ async function code(){
         let tr = document.createElement("tr")
         let td1 = document.createElement("td")
         let td2 = document.createElement("td")
+        let td3 = document.createElement("td")
         
         td1.innerText = data
         td2.innerText = dataJSON.dataset.value[i]
+        td3.innerText = dataJSON2.dataset.value[i]
 
         tr.appendChild(td1)
         tr.appendChild(td2)
+        tr.appendChild(td3)
         if (i%2==0) {
             tr.className = "other"
         }
